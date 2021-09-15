92e1989008907e900388f6402ac49a7971d8eab4
Yeferson Rojas Olcunche <yefersonolcunche@gmail.com>
2021-09-14 21:30:48 -0500

    New proyect

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ const panels = document.querySelectorAll('.panel')
 
 
 
-panels.forEach(panel => {
+panels.forEach((panel) => {
     //console.log(panel)
     panel.addEventListener('click', () => {
-        removeActiveClasses(); 
+        removeActiveClasses()
         panel.classList.add('active')
-
+        
 
     })
 
 })
 
 function removeActiveClasses(){
-       panels.forEach(panel => {
-         panel.classList.remove('active')  
-       })
+     panels.forEach(panel=>{
+          panel.classList.remove('active')
+     })
 }
